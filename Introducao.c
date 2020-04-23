fd30c689a6a237473010c47d97c9899fc6208aa2
Andre Sousa <andre.sousa@reptek.pt>
2020-04-23 21:41:50 +0100

    Introducao sobre C

---
+++ Introducao.c
@@ -0,0 +1,38 @@
+/*
+
+Primeiramente devemos de começar o programa com os includes
+(Não é preciso saber muito sobre isto por enquanto só mais para a frente)
+Usar estes include sempre no inicio do ficheiro <nome>.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+stdio = Standard Input and Output
+stdlib = Standard Library
+
+
+Logo após os includes teremos a função "main"
+
+int main(){
+	
+	return 0;
+}
+
+
+Não irão precisar saber muito o que seria uma função (Mais para a frente irei explicar)
+
+A função "main" por norma é assim (como está escrito em cima)
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+	
+	return 0;
+}
+
