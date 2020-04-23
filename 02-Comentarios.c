9aba7ef7ceed6ea764751b53e6a851d4e78d9ed1
Andre Sousa <andre.sousa@reptek.pt>
2020-04-23 22:02:26 +0100

    Comentarios

---
+++ 02-Comentarios.c
@@ -0,0 +1,26 @@
+/*
+
+
+Por norma aconcelho a usarem bastantes comentarios no vosso codigo para não se perderem no codigo
+
+Na linguagem C os comentarios são feitos apartir de duas "/" ex:
+
+
+	//isto é um comentario não irá ser lido ao rodar o programa
+
+Como diz ai ao fazerem os comentarios ao rodarem o programa esse mesmo comentario não será lido ao rodar o programa
+
+Ao usarem comentarios irão deixar o codigo muito mais legivel, exemplo assim qualquer pessoa que visse o codigo iria entender por causa das ajudas dos comentarios
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	//isto é um comentario não irá ser lido ao rodar o programa
+
+	return 0;
+}
